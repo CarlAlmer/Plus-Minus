f2396d006f64ac36df42325b6a7e868b6e9db7a0
ablair277 <145715304+ablair277@users.noreply.github.com>
2026-04-23 08:34:36 -0500

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -24,7 +24,7 @@ st.divider()
 # -------------------------------------------------------------------------
 # TABS
 # -------------------------------------------------------------------------
-games_tab, players_tab, lineups_tab = st.tabs(["Games", "Players", "Lineups"])
+games_tab, players_tab, lineups_tab, player_stats_tab = st.tabs(["Games", "Players", "Lineups", "Player Stats"])
  
 with games_tab:
     st.header("Games")
@@ -37,3 +37,7 @@ with players_tab:
 with lineups_tab:
     st.header("Lineups")
     st.info("5-man lineup tracking will go here.")
+
+with player_stats_tab:
+ st.header("Player Stats")
+ st.info("Player Stats will go here.")
